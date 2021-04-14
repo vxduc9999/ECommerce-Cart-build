2babb72a708782b4cdbbe2874659250a06ee60df
nhnghia1119 <nhnghia1119@gmail.com>
2021-04-14 17:51:47 +0700

    update order

--- package/frontend/src/components/OrderItem/OrderItem.jsx
+++ package/frontend/src/components/OrderItem/OrderItem.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import "./OrderItem.css";
 
 function OrderItem({ item }) {
+  console.log("🚀 ---------------------------------------------");
+  console.log("🚀 ~ file: OrderItem.jsx ~ line 6 ~ item", item);
+  console.log("🚀 ---------------------------------------------");
   return (
     <div className="oderItem">
       <div className="oderItem__image">
